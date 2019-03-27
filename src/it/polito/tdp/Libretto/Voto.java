3ab9e668b845f807a410387beed6da1efba67cc2
Admin <Admin@DESKTOP-CDNDR7B>
2019-03-27 16:15:26 +0100

    Aggiunti metodi getters, setters, costruttore

---
+++ src/it/polito/tdp/Libretto/Voto.java
@@ -0,0 +1,11 @@
+package it.polito.tdp.Libretto;
+
+import java.time.LocalDate;
+
+public class Voto{
+	private int voto; 
+	private String corso; 
+	private LocalDate data; 	// altre classi per gestire le date Date, Calendar
+	
+	
+}
