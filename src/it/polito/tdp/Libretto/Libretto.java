d50cb93d2a805ce398a1d9b8c655d74178cd5b60
Admin <Admin@DESKTOP-CDNDR7B>
2019-03-27 17:54:45 +0100

    Implementato punto 4

--- src/it/polito/tdp/Libretto/Libretto.java
+++ src/it/polito/tdp/Libretto/Libretto.java
@@ -56,11 +56,47 @@ public class Libretto {
 	 * @return il {@link Voto} corrispondente, oppure {@code null} se non esistente
 	 */
 	public Voto cercaEsame(String nomeEsame) {
-		for(Voto v:this.voti) {
+		
+		/*for(Voto v:this.voti) {
 			if(v.getCorso().equals(nomeEsame)) {	//con == viene confrontata la posizione in memoria dei due oggetti,
 				return v; 							//compareTo lo uso solo quando gli oggetti che sono confrontati sono ordinabili
 			}
-		}
-		return null; 
+		}*/ 
+		
+		Voto voto=new Voto(0, nomeEsame, null); 	//abbiamo costruito il metodo equals solo per il nome del corso
+		int pos=this.voti.indexOf(voto); 			//in questo modo quando cerco la posizione dell'esame cercato nell'array, considero solo 
+													//il campo relativo al nome del corso, fa niente se gli altri campi sono 0 o null
+		if(pos==-1)
+			return null;
+		else 
+			return this.voti.get(pos); 
 	}
-}
+	
+	/**
+	 * Dato un {@link Voto} determina se esiste già un voto con uguale vorso ed uguale punteggio
+	 * @param v
+	 * @return 	{@code true} se ha trovato un corso e punteggio uguali, 
+	 * 			{@code false} se non ha trovato il corso, oppure l'ha trovato con un voto diverso
+	 */
+	public boolean esisteGiaVoto(Voto v) {
+		
+		/* 
+		 * int pos=this.voti.indexOf(v);
+		 * if(pos==-1)
+		 *	 return false; 
+		 * else {
+		 *	 return (v.getPunti()==this.voti.get(pos).getPunti()); 
+		 * }
+		 * */
+		
+		Voto trovato=this.cercaEsame(v.getCorso()); 
+		if(trovato==null)
+			return false; 
+		if(trovato.getPunti()==v.getPunti()) {
+			return true; 
+		} else {
+			return false; }
+		}
+		
+	}
+
